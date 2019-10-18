0aaf9f1bd5cc7eccd5266cc307fbbac3843e65cd
Spencer Bouck <10spen11@gmail.com>
2019-10-18 16:22:47 -0700

    Added File Wipe

--- rainbowGen.c
+++ rainbowGen.c
@@ -13,7 +13,8 @@ char pass[5]; // 4 character password, with null at end for printing
 char hash[8]; // 8 character password
 int intHash; // int representation of the hash
 int *hash_pointer, * pass_pointer, * temp;
-int n, result, *temp;
+int n, *temp;
+unsigned int result;
 MD5_CTX mdContext;  // needed to compute MD5
 
 
@@ -33,17 +34,17 @@ void store(char* pass, int hash)
 }
 
 
-int getHash()
+unsigned int getHash(int * pass_pointer)
 {
 	MD5Init(&mdContext);  // compute MD5 of password
 	MD5Update(&mdContext, pass_pointer, 4);
 	MD5Final(&mdContext);
-	temp = (int*)& mdContext.digest[12];
+	temp = (unsigned int*)& mdContext.digest[12];
 
 	return *temp;
 }
 
-check_hash(char * hash)
+void check_hash(char * hash)
 {
   int i = 0;
   for (i=0;i<8;i++) { 
@@ -58,23 +59,28 @@ check_hash(char * hash)
 // returns the hash that results from the end of a rainbow table
 int getFinalHash(char* pass)
 {
-	int hash = 0;
+	unsigned int hash = 0;
 	char tempPass[5];
 	for (int i = 0; i <= 4; ++i)
 		tempPass[i] = pass[i];
-	
+
+	pass_pointer = (int*)tempPass; // get an int pointer to the password store
+
+
 	for (int i = 0; i < TABLE_WIDTH; ++i)
 	{
-		hash = getHash();
+		hash = getHash(pass_pointer);
 		inverseHash(hash, tempPass);
 	}
 
 	return hash;
 }
 
-main(int argc, char *argv[])
+void main(int argc, char *argv[])
 {
-    pass_pointer = (int*)pass; // get an int pointer to the password store
+	FILE* fptr;
+	fptr = fopen("./rainbow-hashes.txt", "w");
+	fclose(fptr);
 
     for (int i = 0; i < 4; ++i)
     {
@@ -86,7 +92,7 @@ main(int argc, char *argv[])
 	for (int i = 0; i < TABLE_HEIGHT; ++i)
 	{
 		result = getFinalHash(pass); // result is 32 bits of MD5 -- there is a BUG here, oh well.
-		store(&pass, result);
+		store(pass, result);
 		carries = increment_pass(pass);
 	}
 
